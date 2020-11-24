c98f38f92422f5ba2ab1eadfd859af0882af7abb
jschmitt <jasonschmittcode@gmail.com>
2020-11-24 14:21:56 -0500

    adding materialize and blank slate for routing

--- src/App.js
+++ src/App.js
@@ -3,25 +3,7 @@ import "./App.css";
 import Test from "./pages/test";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Test />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">wow we are here</div>;
 }
 
 export default App;
