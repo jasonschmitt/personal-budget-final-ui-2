5e2010b369a07dc3af3f89c290eae1ddd4067fc2
jschmitt <jasonschmittcode@gmail.com>
2020-11-24 15:15:19 -0500

    adding signup page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Test from "./pages/test";
 import TestTwo from "./pages/testtwo";
+import Signup from "./pages/signup";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
+  console.log("app");
+  axios
+    .get(`http://localhost:8081/test`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   return (
     <Router>
       <Switch>
@@ -13,6 +25,9 @@ function App() {
         </Route>
         <Route exact path="/testtwo">
           <TestTwo />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/signup.js
@@ -0,0 +1,83 @@
+import React from "react";
+import axios from "axios";
+
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { firstName: "", password: "", email: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    // console.log(target);
+    // console.log(target.name);
+    const name = target.name;
+    this.setState({ [name]: event.target.value });
+    // console.log(this.state);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const dataObj = {
+      firstName: this.state.firstName,
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("http://localhost:8081/auth/register", dataObj)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+        >
+          Submit
+          <i className="material-icons right">send</i>
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Signup;
