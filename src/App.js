382c3d1c1cfc634cd2c15fcc0891f1690a9e320e
jschmitt <jasonschmittcode@gmail.com>
2020-11-24 17:27:31 -0500

    fixing url typo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   let baseURL =
     window.location.hostname === "localhost"
       ? "http://localhost:8081"
-      : "https://personal-budget-api-api.herokuapp.com";
+      : "https://personal-budget-api.herokuapp.com";
 
   axios
     .get(`${baseURL}/test`)
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -24,7 +24,7 @@ class Signup extends React.Component {
     let baseURL =
       window.location.hostname === "localhost"
         ? "http://localhost:8081"
-        : "https://personal-budget-api-api.herokuapp.com";
+        : "https://personal-budget-api.herokuapp.com";
 
     const dataObj = {
       firstName: this.state.firstName,
