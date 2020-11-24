62184aeb5cad83a57b19791f1948c36333c83669
jschmitt <jasonschmittcode@gmail.com>
2020-11-24 14:49:36 -0500

    adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Test from "./pages/test";
+import TestTwo from "./pages/testtwo";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">wow we are here</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Test />
+        </Route>
+        <Route exact path="/testtwo">
+          <TestTwo />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/testtwo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class TestTwo extends React.Component {
+  render() {
+    return <div>test two page :)</div>;
+  }
+}
+
+export default TestTwo;
