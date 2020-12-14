266b277f0632b85367aa061519a4f9751ef06787
jschmitt <jasonschmittcode@gmail.com>
2020-12-14 18:30:41 -0500

    adding in stub data for chart.js chart

--- src/components/chartOne/chartOne.js
+++ src/components/chartOne/chartOne.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import Chart from 'chart.js'
 
 class ChartOne extends React.Component {
+  componentDidMount() {
+    console.log('chart one mounted')
+    this.renderChart()
+  }
+
+  renderChart() {
+    var ctx = document.getElementById('myChart').getContext('2d')
+    var data = {
+      datasets: [
+        {
+          data: [10, 20, 30],
+          backgroundColor: ['red', 'yellow', 'blue'],
+        },
+      ],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: ['Red', 'Yellow', 'Blue'],
+    }
+    var myDoughnutChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: {
+        maintainAspectRatio: false,
+      },
+    })
+  }
+
   render() {
-    return <div>in here we will render the first chart using chart.js</div>
+    return (
+      <section className="chartOne">
+        <canvas id="myChart" width="200" height="200"></canvas>
+      </section>
+    )
   }
 }
 
