3e6a8d28fca5e7719c616c9238ad0b8129f6d868
jschmitt <jasonschmittcode@gmail.com>
2020-12-16 21:22:01 -0500

    reading in data from database and populating chart on dashboard page

--- src/components/chartOne/chartOne.js
+++ src/components/chartOne/chartOne.js
@@ -1,24 +1,43 @@
 import React from 'react'
 import Chart from 'chart.js'
+import axios from 'axios'
 
 class ChartOne extends React.Component {
   componentDidMount() {
     console.log('chart one mounted')
-    this.renderChart()
+    // get data with axios request
+    console.log('making axios request')
+    let baseURL =
+      window.location.hostname === 'localhost'
+        ? 'http://localhost:8081'
+        : 'https://personal-budget-api.herokuapp.com'
+
+    const that = this
+    axios
+      .get(`${baseURL}/budget`)
+      .then(function (response) {
+        // console.log(response.data)
+        // pass data to renderChart function
+        that.renderChart(response.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
   }
 
-  renderChart() {
+  renderChart(apiResponse) {
+    console.log(apiResponse)
     var ctx = document.getElementById('myChart').getContext('2d')
     var data = {
       datasets: [
         {
-          data: [10, 20, 30],
-          backgroundColor: ['red', 'rgb(15,117,67)', 'rgb(24,47,234)'],
+          data: apiResponse[0].values,
+          backgroundColor: apiResponse[0].backgroundColor,
         },
       ],
 
       // These labels appear in the legend and in the tooltips when hovering different arcs
-      labels: ['Red', 'Yellow', 'rgb(24,47,234)'],
+      labels: apiResponse[0].labels,
     }
     var myDoughnutChart = new Chart(ctx, {
       type: 'doughnut',
