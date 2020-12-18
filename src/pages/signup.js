5c16cf2da881768798641cb51c220759e50add92
jschmitt <jasonschmittcode@gmail.com>
2020-12-17 21:18:30 -0500

    adding json web token id and expiration to local storage when user originally signs up

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,44 +1,55 @@
-import React from "react";
-import axios from "axios";
+import React from 'react'
+import axios from 'axios'
 
 class Signup extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { firstName: "", password: "", email: "" };
+    super(props)
+    this.state = { firstName: '', password: '', email: '' }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
-    const target = event.target;
+    const target = event.target
     // console.log(target);
     // console.log(target.name);
-    const name = target.name;
-    this.setState({ [name]: event.target.value });
+    const name = target.name
+    this.setState({ [name]: event.target.value })
     // console.log(this.state);
   }
 
   handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     let baseURL =
-      window.location.hostname === "localhost"
-        ? "http://localhost:8081"
-        : "https://personal-budget-api.herokuapp.com";
+      window.location.hostname === 'localhost'
+        ? 'http://localhost:8081'
+        : 'https://personal-budget-api.herokuapp.com'
 
     const dataObj = {
       firstName: this.state.firstName,
       email: this.state.email,
       password: this.state.password,
-    };
+    }
     axios
       .post(`${baseURL}/auth/register`, dataObj)
       .then(function (response) {
-        console.log(response);
+        console.log(response)
+        localStorage.setItem('token', response.data.token)
+        console.log(typeof response.data._id)
+        localStorage.setItem('_id', response.data._id)
+        console.log('setExpirationInLocalStorage')
+        var expiration = Date.now()
+        var oneMinuteInMilliseconds = 60000
+        // console.log(expiration + oneMinuteInMilliseconds);
+        // console.log(expiration);
+        expiration = expiration + oneMinuteInMilliseconds
+        localStorage.setItem('expire', expiration)
+        window.location.href = '/dashboard'
       })
       .catch(function (error) {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
   render() {
@@ -80,8 +91,8 @@ class Signup extends React.Component {
           <i className="material-icons right">send</i>
         </button>
       </form>
-    );
+    )
   }
 }
 
-export default Signup;
+export default Signup
